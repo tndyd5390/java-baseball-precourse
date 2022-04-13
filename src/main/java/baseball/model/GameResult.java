9cf0279528aa5e5d5962d5121f43d152c06d3487
waterdragon <tndyd5390@naver.com>
2022-04-13 22:02:59 +0900

    feat: 게임 결과 문장 생성을 위한 객체 구현
    - BallCountMap에서 집계된 스트라이크와 볼을 가지고 사용자에게 보여줄 문자열을 생성하는 객체입니다.

---
+++ src/main/java/baseball/model/GameResult.java
@@ -0,0 +1,40 @@
+package baseball.model;
+
+public class GameResult {
+    private int strike;
+    private int ball;
+
+    public GameResult(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public String getGameResultStr() {
+        if (isNothing()) {
+            return getNothingStr();
+        }
+        return getBallStr() + getStrikeStr();
+    }
+
+    private String getStrikeStr() {
+        if (strike != 0) {
+            return strike + BallCount.STRIKE.getBallCount() + " ";
+        }
+        return "";
+    }
+
+    private String getBallStr() {
+        if (ball != 0) {
+            return ball + BallCount.BALL.getBallCount() + " ";
+        }
+        return "";
+    }
+
+    private String getNothingStr() {
+        return "낫싱";
+    }
+
+    private boolean isNothing() {
+        return strike == 0 && ball == 0;
+    }
+}
---
+++ src/test/java/baseball/model/GameResultTest.java
@@ -0,0 +1,78 @@
+package baseball.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("게임 결과 문자열 생성 객체 단위 테스트")
+class GameResultTest {
+
+    @DisplayName("객체 생성 테스트")
+    @Test
+    void newTest() throws Exception {
+
+        //given //when
+        GameResult gameResult = new GameResult(1, 2);
+
+        //then
+        assertThat(gameResult).isInstanceOf(GameResult.class);
+    }
+
+    @DisplayName("게임 결과 문자열 테스트(1스트라이크 1볼)")
+    @Test
+    void gameResultStr1Strike1Ball() throws Exception {
+
+        //given //when
+        GameResult gameResult = new GameResult(1, 1);
+
+        //when
+        String resultStr = gameResult.getGameResultStr();
+
+        //then
+        assertThat(resultStr).isEqualTo("1볼 1스트라이크 ");
+    }
+
+    @DisplayName("게임 결과 문자열 테스트(낫싱)")
+    @Test
+    void gameResultStrNothingTest() throws Exception {
+
+        //given //when
+        GameResult gameResult = new GameResult(0, 0);
+
+        //when
+        String resultStr = gameResult.getGameResultStr();
+
+        //then
+        assertThat(resultStr).isEqualTo("낫싱");
+
+    }
+
+    @DisplayName("게임 결과 문자열 테스트(1스트라이크)")
+    @Test
+    void gameResultStr1StrikeTest() throws Exception {
+
+        //given //when
+        GameResult gameResult = new GameResult(1, 0);
+
+        //when
+        String resultStr = gameResult.getGameResultStr();
+
+        //then
+        assertThat(resultStr).isEqualTo("1스트라이크 ");
+    }
+
+    @DisplayName("게임 결과 문자열 테스트(1볼)")
+    @Test
+    void gameResultStr1BallTest() throws Exception {
+
+        //given //when
+        GameResult gameResult = new GameResult(0, 1);
+
+        //when
+        String resultStr = gameResult.getGameResultStr();
+
+        //then
+        assertThat(resultStr).isEqualTo("1볼 ");
+    }
+}
