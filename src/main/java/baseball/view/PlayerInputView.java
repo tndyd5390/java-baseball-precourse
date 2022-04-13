18e125ee5b39538cd059d0940187893db5a5a16d
waterdragon <tndyd5390@naver.com>
2022-04-13 22:09:41 +0900

    feat: 게임 진행을 위한 view 구현

---
+++ src/main/java/baseball/view/PlayerInputView.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class PlayerInputView {
+    public static void printGameResult(String result) {
+        System.out.println(result);
+    }
+
+    public static void printGameExit() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
