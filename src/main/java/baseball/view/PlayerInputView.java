bea76178ed9b620fb835e97be1e5b3c4442c528b
waterdragon <tndyd5390@naver.com>
2022-04-13 23:06:31 +0900

    fix: view 객체 이름 수정
    - inputView와 outputView의 이름을 바꿔서 적는 실수가 있었습니다.

--- src/main/java/baseball/view/PlayerInputView.java
+++ src/main/java/baseball/view/PlayerInputView.java
@@ -1,11 +1,15 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class PlayerInputView {
-    public static void printGameResult(String result) {
-        System.out.println(result);
+    public static String inputNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
     }
 
-    public static void printGameExit() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    public static String inputGameRestartOrExit() {
+        System.out.println("게임을 새로 시작하려면1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
     }
 }
